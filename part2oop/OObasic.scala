b357b5e904dd37d3fa74af46adb0b3ce93cd28d0
Yurii Markov <alegoriewp@gmail.com>
2020-07-31 12:06:57 +0300

    2 simple exercises. 1. Classes novel and Author 2. Counter methods

--- part2oop/OObasic.scala
+++ part2oop/OObasic.scala
@@ -6,6 +6,18 @@ object OObasic extends App {
   println(person.x)
   person.greet("Vitalii")
 
+  val writer = new Author("Andrew", "Lock", 1801)
+  val novel = new Novel("MyStory", 1861, writer)
+
+
+  println(novel.isWrittenBy(writer))
+  println(novel.authorAge)
+
+
+  val counter = new Counter
+  counter.inc.print
+  counter.inc.inc.inc.print
+  counter.inc(10).print
 }
 
 // constructor
@@ -33,3 +45,56 @@ class Person(name: String, val age: Int = 0, nationality: String) {
 }
 
 // class parameters are NOT Fields
+
+
+/*implement Novel and Author class
+writer: (name, surname, year of birth):
+- method fullname (concat name + surname)
+novel: (name year of release, author):
+- author age - age of author where novel released
+- isWrittenBy (author)
+- copy (new year of release) = new instance  of novel*/
+
+
+class Author(name: String = "Unknown", surname: String = "Unknown", val year_of_birth: Int = 1980) {
+  def fullname: String = name + " " + surname
+}
+
+class Novel(name: String = "Unknown", year_of_release: Int = 1900, author: Author) {
+  def authorAge: Int = year_of_release - author.year_of_birth
+
+  def isWrittenBy(author: Author) = author.fullname == this.author
+
+  def copy(newYear: Int): Novel = new Novel(name, newYear, author)
+
+}
+
+/* Counter class
+  - receive int
+  - method current count
+  - method to increment
+  - overload inc/dec to receive an amount
+ */
+
+class Counter(val count: Int = 0) {
+  def inc = {
+    println("Incrementing")
+    new Counter(count + 1)
+  } // immutability
+  def dec = {
+    println("Decrementing")
+    new Counter(count - 1)
+  }
+
+  def inc(n: Int): Counter = {
+    if (n <= 0) this
+    else inc.inc(n - 1)
+  }
+
+  def dec(n: Int): Counter = {
+    if (n <= 0) this
+    else dec.dec(n - 1)
+  }
+
+  def print = println(count)
+}
